44c1f2f20e68250dc2a74fa5106160c3b95fd740
PNiyonkuru <pniyonku@gmail.com>
2025-10-26 10:53:53 -0500

    added logo

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,24 +1,31 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NavBar() {
   return (
     <header className="sticky top-0 z-10 backdrop-blur bg-[#0f0a19]/70 border-b border-white/10">
       <div className="max-w-6xl mx-auto px-6 py-4 flex items-center w-full justify-between">
 
-    <Link href="/" className="flex items-center gap-2 cursor-pointer">
-      <div className="h-8 w-8 rounded-lg bg-gradient-to-br from-purple-500 to-blue-600" />
-      <span className="text-lg font-bold tracking-tight">WeCode</span>
-    </Link>
+        <Link href="/" className="flex items-center gap-2 cursor-pointer">
+          <div className="relative h-16 w-48 rounded-lg  overflow-hidden">
+            <Image
+              src="/LogoImage.png"
+              alt="Collabo Logo"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </Link>
 
-    <div className="flex w-full max-w-lg justify-between">
-      <NavLink href="/ourTeam">Our Team</NavLink>
-      <NavLink href="/aboutUs">About Us</NavLink>
-      <NavLink href="https://github.com">Github</NavLink>
-    </div>
+        <div className="flex w-full max-w-lg justify-between">
+          <NavLink href="/ourTeam">Our Team</NavLink>
+          <NavLink href="/aboutUs">About Us</NavLink>
+          <NavLink href="https://github.com">Github</NavLink>
+        </div>
 
-  </div>
+      </div>
     </header>
   );
 }
